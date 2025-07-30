b9a2550631d13b8c2e82884c7706a2f1027e5567
rifayi <rifayiavayil@gmail.com>
2025-07-30 10:48:39 +0530

    very basic private routing for shop page  via authentication with localstorage

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import BlackContainer from "../components/BlackContainer";
 import ProductSection from "../components/ProductSection";
 import ShopHeroSection from "../components/ShopHeroSection";
+import { useEffect, useState } from "react";
 
 export default function Shop() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("username")) {
+      navigate("/signup");
+    } else {
+      setLoading(true);
+    }
+  }, []);
+  if (!loading) return <h1 className="text-6xl">Loading.......</h1>;
   return (
     <>
       <ShopHeroSection />
